40c23738b61d7d2680db2f8c71b84162c724e9c2
Rodrigovssis <rodrigoassiscontato@gmail.com>
2024-07-01 18:55:23 -0300

    criando contagem

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -584,7 +584,27 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"3cYfC":[function(require,module,exports) {
-console.log("Ol\xe1 mundo");
+AOS.init();
+const dataDoEvento = new Date("Dec 12, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+    const diaEmMs = 86400000;
+    const horaEmMs = 3600000;
+    const minutoEmMs = 60000;
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % diaEmMs / horaEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horaEmMs / minutoEmMs);
+    const segundosAteOEvento = Math.floor(distanciaAteOEvento % minutoEmMs / 1000);
+    console.log(diasAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteOEvento);
+    document.getElementById("contador").innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+    if (distanciaAteOEvento < 0) clearInterval(contaAsHoras).innerHTML = "Evento expirado";
+}, 1000);
 
 },{}]},["8bDoD","3cYfC"], "3cYfC", "parcelRequiredb18")
 
